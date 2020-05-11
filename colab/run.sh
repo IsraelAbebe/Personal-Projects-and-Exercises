d1592b57f59ee365b65d7edf1147f325cf374482
Israel Abebe <se.israel.abebe@gmail.com>
2020-05-11 12:39:39 +0000

    added colab

---
+++ colab/run.sh
@@ -0,0 +1,15 @@
+#python
+sudo apt-get install software-properties-common
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt-get update
+sudo apt-get install python3.6
+
+
+
+#make pytohn 3 default
+alias python=python3
+
+
+#install ML libraries
+pip install torch torchvision
+pip install PIL
