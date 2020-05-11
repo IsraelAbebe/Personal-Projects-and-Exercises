89a2bcf2ec982006e1737a0a85e6223bd2746f9a
Israel Abebe <se.israel.abebe@gmail.com>
2020-05-11 12:42:34 +0000

    Update run.sh

--- colab/run.sh
+++ colab/run.sh
@@ -1,4 +1,3 @@
-#python
 sudo apt-get install software-properties-common
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt-get update
@@ -6,10 +5,8 @@ sudo apt-get install python3.6
 
 
 
-#make pytohn 3 default
 alias python=python3
 
 
-#install ML libraries
 pip install torch torchvision
 pip install PIL
